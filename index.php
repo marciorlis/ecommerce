5a8893a3543ae8d87964f4e358330888c7fa9f5a
marciorlis <ecumenico@gmail.com>
2019-02-21 11:18:26 -0300

    criamos page admin

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
-echo $_SERVER['DOCUMENT_ROOT']."/views/";
-echo "////////////////////-----///////////////////////////";
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -13,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     // inicio do que esta dentro da rota
 	$page = new Page();
+
+	$page->setTpl("index");
+	// fim do que esta dentro da rota
+});
+
+$app->get('/admin', function() {
+    // inicio do que esta dentro da rota
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 	// fim do que esta dentro da rota
